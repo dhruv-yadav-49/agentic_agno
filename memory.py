ec40dfd09d7b95e8964ac31f990da960871941bd
dhruv-yadav-49 <dhruvofficial73@gmail.com>
2026-06-10 01:22:14 +0530

    done

---
+++ memory.py
@@ -0,0 +1,35 @@
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+from agno.agent import Agent
+from agno.models.groq import Groq
+from dotenv import load_dotenv
+from agno.db.sqlite import SqliteDb
+from rich.pretty import pprint
+
+load_dotenv()
+
+db = SqliteDb(db_file="agno.db")
+db.clear_memories()
+
+def build_agent():
+    return Agent(
+        db=db,
+        markdown=True,
+        add_history_to_context=True,
+        enable_user_memories=True
+    )
+
+agent = build_agent()
+
+user_id = "rahul@gmail.com"
+agent.print_response("I am Dhruv & I am a Data Analyst.", user_id=user_id)
+agent.print_response("who am I?", user_id=user_id)
+
+memories = agent.get_user_memories(
+    user_id=user_id
+)
+
+print("MEMORIES: ")
+pprint(memories)
